572becc473bf536e70dcdc2fb9e76f311c18c15c
Ian <itmacleod@email.wm.edu>
2022-04-29 07:30:23 -0400

    Set turtle to lower left of screen

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import random
 import turtle as t
 from turtle import Screen
 
-
 """
 colors = colorgram.extract("image.png", 30)
 color_list = []
@@ -16,6 +15,9 @@ for color in colors:
 
 print(color_list)
 """
+screen = Screen()
+screen.setworldcoordinates(-1, -1, screen.window_width() - 1, screen.window_height() - 1)
+
 color_list = [(200, 167, 110), (237, 241, 246), (144, 74, 52), (169, 152, 45), (58, 92, 119), (224, 203, 131), (136, 162, 180), (131, 34, 26), (51, 117, 89), (199, 94, 72), (143, 25, 30), (18, 97, 74), (69, 47, 40), (173, 146, 153), (150, 177, 152), (131, 70, 74), (56, 43, 46), (237, 174, 163), (184, 88, 94), (38, 58, 71), (28, 82, 89), (182, 204, 178), (242, 156, 160), (93, 144, 124), (20, 66, 57), (36, 65, 96), (108, 125, 154)]
 turtle = t.Turtle()
 turtle.speed("fastest")
@@ -30,6 +32,4 @@ while count < 100:
     count += 1
 
 
-
-screen = Screen()
 screen.exitonclick()
