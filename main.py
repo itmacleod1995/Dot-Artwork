7374b313ce5f8a82896fbb0689516ce01f800230
Ian <itmacleod@email.wm.edu>
2022-04-29 07:53:44 -0400

    Made first row of random colored dots

--- main.py
+++ main.py
@@ -16,18 +16,21 @@ for color in colors:
 print(color_list)
 """
 screen = Screen()
-screen.setworldcoordinates(-1, -1, screen.window_width() - 1, screen.window_height() - 1)
+screen.setworldcoordinates(-1, -1, screen.window_width() - 5, screen.window_height() - 1)
 
 color_list = [(200, 167, 110), (237, 241, 246), (144, 74, 52), (169, 152, 45), (58, 92, 119), (224, 203, 131), (136, 162, 180), (131, 34, 26), (51, 117, 89), (199, 94, 72), (143, 25, 30), (18, 97, 74), (69, 47, 40), (173, 146, 153), (150, 177, 152), (131, 70, 74), (56, 43, 46), (237, 174, 163), (184, 88, 94), (38, 58, 71), (28, 82, 89), (182, 204, 178), (242, 156, 160), (93, 144, 124), (20, 66, 57), (36, 65, 96), (108, 125, 154)]
 turtle = t.Turtle()
-turtle.speed("fastest")
+turtle.speed("slow")
+screen.colormode(255)
 
 
 count = 0
 while count < 100:
-    turtle.circle(10)
+    color = random.choice(color_list)
+    #turtle.color(color)
+    turtle.dot(17, color)
     turtle.penup()
-    turtle.forward(20)
+    turtle.forward(80)
     turtle.pendown()
     count += 1
 
